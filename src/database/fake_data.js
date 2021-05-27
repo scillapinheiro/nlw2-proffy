f6fe6b4cce984b178efa9f3feb2d40cca03bbd78
Priscilla Pinheiro <priscilla_pinheiro1@hotmail.com>
2021-05-27 15:59:22 -0300

    NLW2 Proffy versão 1

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+// Dados
+const proffys = [
+    { 
+        name: "Diego Fernandes",
+        avatar: "https://avatars.githubusercontent.com/u/2254731?v=4",
+        whatsapp: "988888888",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220] 
+    },
+    { 
+        name: "Mayk Brito",
+        avatar: "https://avatars.githubusercontent.com/u/6643122?v=4",
+        whatsapp: "988888888",
+        bio: "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.<br><br>Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: Aprenda a fazer dinheiro com isso!",
+        subject: "Educação Física",
+        cost: "40",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220] 
+    },
+    { 
+        name: "Tiago Luchtenberg",
+        avatar: "https://mir-s3-cdn-cf.behance.net/user/115/4713a37191453.5f14e9ebb5e58.jpg",
+        whatsapp: "988888888",
+        bio: "Às vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido.<br><br>Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida",
+        subject: "Geografia",
+        cost: "360",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220] 
+    }
+]
